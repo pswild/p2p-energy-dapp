c31c939561886c96e36083c1ec345ea0cac3fe55
Parker Wild <parkerwild96@gmail.com>
2019-03-09 16:49:05 -0500

    Web3 failure.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
+// Web3.
+import getWeb3 from "./util/web3/getWeb3";
+
+// Contracts.
+import SimpleStorageContract from "../contracts/SimpleStorage.sol";
+
 // UI Components
 import LoginButtonContainer from './user/ui/loginbutton/LoginButtonContainer'
 import LogoutButtonContainer from './user/ui/logoutbutton/LogoutButtonContainer'
@@ -13,7 +19,66 @@ import './css/pure-min.css'
 import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+      super(props)
+      this.state= {
+        storageValue: 0,
+        web3: null,
+        accounts: null,
+        contract: null
+      };
+    }
+
+  async componentDidMount() {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  async runExample() {
+    const {
+      accounts,
+      contract
+    } = this.state;
+
+    // Stores a given value, 5 by default.
+    await contract.methods.set(5).send({ from: accounts[0] });
+
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
+
   render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+
     const OnlyAuthLinks = VisibleOnlyAuth(() =>
       <span>
         <li className="pure-menu-item">
@@ -44,6 +109,16 @@ class App extends Component {
             <OnlyAuthLinks />
           </ul>
         </nav>
+
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 40</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
 
         {this.props.children}
       </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { UserIsAuthenticated } from './util/wrappers.js'
+import getWeb3 from './util/web3/getWeb3'
 
 // Layouts
 import App from './App'
@@ -15,6 +16,15 @@ import Profile from './user/layouts/profile/Profile'
 import store from './store'
 
 const history = syncHistoryWithStore(browserHistory, store)
+
+// Initialize web3 and set in Redux.
+getWeb3
+.then(results => {
+  console.log('Web3 initialized!')
+})
+.catch(() => {
+  console.log('Error in web3 initialization.')
+})
 
 ReactDOM.render((
     <Provider store={store}>
