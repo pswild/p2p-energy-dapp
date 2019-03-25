0c519f7f4bbab7cedbe473bcd8cca336c7630d34
Vignesh Rajendran <vpr@princeton.edu>
2019-03-24 21:06:38 -0400

    Add files via upload

--- contracts/Auction1B1P.sol
+++ contracts/Auction1B1P.sol
@@ -1,12 +1,14 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.23;
 
 // THIS IS A SEALED-BID FIRST PRICE AUCTION
 contract Auction1B1P {
+
+  //string public description;
   //string public instructions; // will be used for delivery address or email
   uint public utilityBuyBackRate;
   bool public initialPrice = true; // at first asking price is OK, then +25% required
-  uint public timestampEnd;
-  address public beneficiary;
+  //uint public timestampEnd;
+  //address public beneficiary;
   bool public finalized = false;
 
   address public owner;
@@ -18,13 +20,13 @@ contract Auction1B1P {
   uint public increaseTimeIfBidBeforeEnd = 24 * 60 * 60;
   uint public increaseTimeBy = 24 * 60 * 60;
 
-  event BidEvent(address indexed bidder, uint value, uint timestamp); // cannot have event and struct with the same name
-  event Refund(address indexed bidder, uint value, uint timestamp);
+  event BidEvent(address indexed bidder, uint value);//, uint timestamp); // cannot have event and struct with the same name
+  event Refund(address indexed bidder, uint value);//, uint timestamp);
 
 
   modifier onlyOwner { require(owner == msg.sender, "only owner"); _; }
   modifier onlyWinner { require(winner == msg.sender, "only winner"); _; }
-  modifier ended { require(now > timestampEnd, "not ended yet"); _; }
+  //modifier ended { require(now > timestampEnd, "not ended yet"); _; }
 
 
   //function setDescription(string _description) public onlyOwner() {
@@ -37,13 +39,15 @@ contract Auction1B1P {
   //}
 
   //constructor(uint _price, string _description, uint _timestampEnd, address _beneficiary) public {
-  constructor(uint _utilityBuyBackRate, uint _timestampEnd, address _beneficiary) public {
-    require(_timestampEnd > now, "end of the auction must be in the future");
+  //constructor(uint _utilityBuyBackRate, uint _timestampEnd, address _beneficiary) public {
+    //require(_timestampEnd > now, "end of the auction must be in the future");
+  constructor() public {
     owner = msg.sender;
-    utilityBuyBackRate = _utilityBuyBackRate;
+    //utilityBuyBackRate = _utilityBuyBackRate;
+    utilityBuyBackRate = 1;
     //description = _description;
-    timestampEnd = _timestampEnd;
-    beneficiary = _beneficiary;
+    //timestampEnd = _timestampEnd;
+    //beneficiary = _beneficiary;
   }
 
   // Same for all the derived contract, it's the implementation of refund() and bid() that differs
@@ -56,7 +60,7 @@ contract Auction1B1P {
   }
 
   function bid() public payable {
-    require(now < timestampEnd, "auction has ended"); // sending ether only allowed before the end
+    //require(now < timestampEnd, "auction has ended"); // sending ether only allowed before the end
     require(bids[msg.sender] == 0, "you can only bid once");
 
 
@@ -65,9 +69,9 @@ contract Auction1B1P {
 
     require(bids[msg.sender] >= utilityBuyBackRate, "bid too low, minimum is the utilityBuyBackRate");
 
-    if (now > timestampEnd - increaseTimeIfBidBeforeEnd) {
-      timestampEnd = now + increaseTimeBy;
-    }
+    //if (now > timestampEnd - increaseTimeIfBidBeforeEnd) {
+      //timestampEnd = now + increaseTimeBy;
+    //}
 
     initialPrice = false;
     //price = bids[msg.sender]; makes the bid the new low, not needed here
@@ -75,15 +79,15 @@ contract Auction1B1P {
       winner = msg.sender;
     }
 
-    emit BidEvent(winner, msg.value, now);
+    emit BidEvent(winner, msg.value);//, now);
   }
 
-  function finalize() public ended() onlyOwner() {
+  function finalize() public {//ended() onlyOwner() {
     require(finalized == false, "can withdraw only once");
     require(initialPrice == false, "can withdraw only if there were bids");
 
     finalized = true;
-    beneficiary.transfer(bids[winner]);
+    //beneficiary.transfer(bids[winner]);
   }
 
   function refund(address addr) private {
@@ -94,7 +98,7 @@ contract Auction1B1P {
     bids[addr] = 0;
     addr.transfer(refundValue);
 
-    emit Refund(addr, refundValue, now);
+    emit Refund(addr, refundValue);//, now);
   }
 
   function refund() public {
@@ -104,4 +108,5 @@ contract Auction1B1P {
   function refundOnBehalf(address addr) public onlyOwner() {
     refund(addr);
   }
+
 }
