46686d47216ba7ce522e78a591da1d7f7b330ebf
Parker Wild <parkerwild96@gmail.com>
2019-04-01 11:00:41 -0400

    Revert to uport-connect@0.7.3.

--- node_modules/uport-transports/src/network/index.js
+++
@@ -1,4 +0,0 @@
-import * as defaults from './defaults.js'
-import * as config from './config.js'
-
-export { defaults, config }
--- node_modules/uport-transports/src/transport/index.js
+++
@@ -1,8 +0,0 @@
-import * as qr from './qr'
-import * as ui from './ui/index'
-import * as messageServer from './messageServer'
-import * as push from './push'
-import * as url from './url'
-import poll from './poll'
-
-export { qr, push, messageServer, poll, url, ui }
