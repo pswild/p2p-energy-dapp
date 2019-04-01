ae9cd4c7227534fadd1878c4ffeb7fad5d2f083c
Parker Wild <parkerwild96@gmail.com>
2019-04-01 10:53:20 -0400

    Revert "Updated contract build addresses."
    This reverts commit 344792ff2175bcd4500cb0703868dd54b794943f.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 var Auction = artifacts.require("./Auction.sol");
-// var Auction1B1P = artifacts.require("./Auction1B1P.sol");
+var Auction1B1P = artifacts.require("./Auction1B1P.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Auction);
-  // deployer.deploy(Auction1B1P);
+  deployer.deploy(Auction1B1P);
   deployer.deploy(SimpleStorage);
 };
