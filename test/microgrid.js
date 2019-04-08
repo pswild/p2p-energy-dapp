e9ec67e9eae0a6c069d22871c296b71f014a57ba
Parker Wild <parkerwild96@gmail.com>
2019-04-08 19:31:49 -0400

    Added SunDance processing to microgrid test.

--- test/microgrid.js
+++ test/microgrid.js
@@ -2,27 +2,98 @@
 // Description: Testing framework for microgrid using real data.
 
 // Contracts.
-// const Contract_Name = artifacts.require('Contract_Name.sol');
+const Auction1B2P = artifacts.require('Auction1B2P.sol');
 
-// D3 CSV Parser.
-// const D3 = require("d3");
+// D3 JavaScript.
+const D3 = require("d3");
 
-// Load usage data.
-// var data = [];
-// for (var i = 1; i <= 100; i++) {
-//   const csv = require('../data/sundance/SunDance_' + i + '.csv');
-//   data.push(csv);
-// }
+////////////////////
+// Data analysis. //
+////////////////////
 
-// Parse CSV file.
-// D3.csv(data).then(function(data) {
-//   // SunDance CSV format: ["date", "use", "gen", "grid"].
-//
-//   // Log.
-//   console.log(data);
-// });
+// SunDance CSV format:
+// ["date", "use", "gen", "grid"].
+
+// Site object format:
+// {"name": file_name, "points": [points]}
+
+// Point object format:
+// {"date": date, "use": use, "gen": gen, grid": grid}
+
+// SunDance.
+var sundance = [];
+
+// Process SunDance data.
+for (var i = 1; i <= 100; i++) {
+  // Filter missing files.
+  if(i == 2 || i == 6) {
+    continue
+  }
+
+  // Site object.
+  let site = new Object();
+  var file_name = '../data/sundance/SunDance_' + i + '.csv';
+  var points = [];
+
+  // Load SunDance data.
+  const csv = require('../data/sundance/SunDance_' + i + '.csv');
+
+  // Parse CSV files.
+  D3.csv(csv).then(function(csv) {
+    // Read each line of CSV.
+    for (var j = 0; j < csv.length; j++) {
+      // Point object.
+      let point = new Object();
+
+      // Create new date object.
+      var csvDate = csv[j].date.split(" ");
+
+      var calendar = csvDate[0].split("/");
+      var period = csvDate[1].split(":");
+      var ampm = csvDate[2];
+
+      var year = "20" + calendar[2];
+      var month = calendar[0];
+      var day = calendar[1];
+
+      var hour = parseInt(period[0], 10);
+      if (ampm === "PM") {
+        hour += 12;
+      }
+
+      var date = new Date(year, month, day, hour);
+
+      // Point properties.
+      point = {
+        date: date,
+        use: csv[j].use,
+        gen: csv[j].gen,
+        grid: csv[j].grid
+      }
+
+      // Add to points.
+      points.push(point);
+    }
+
+    // Site properties.
+    site = {
+      name: file_name,
+      points: points
+    }
+
+    // Add site to SunDance.
+    sundance.push(site);
+  })
+}
+
+// Log.
+// console.log(sundance);
+
+///////////////
+// Microgrid //
+///////////////
 
 // Test microgrid setup using auction contract.
-// contract('Contract_Name', function (accounts) {
-//
-// });
+contract('Auction1B2P', function (accounts) {
+
+});
