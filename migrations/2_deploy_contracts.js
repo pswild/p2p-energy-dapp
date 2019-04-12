5a3fbab8fc5cd23c45cccc58a4ed139de604ab6d
Parker Wild <parkerwild96@gmail.com>
2019-04-12 15:27:42 -0400

    SunDance data cleaned. Auctions removed. Issue with CSV spaces.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,8 @@
 var Auction = artifacts.require("./Auction.sol");
-// var Auction1B1P = artifacts.require("./Auction1B1P.sol");
-// var Auction1B2P = artifacts.require("./Auction1B2P.sol");
-// var AuctionMM = artifacts.require("./AuctionMM.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
-// Global variables.
-// let timestampEnd = web3.eth.getBlock('latest').timestamp + 3600;
-// let wallet = 0x4f6baa92aA8f4732eCf858CC6931d5bfF574A773;
-// let buyback = 1e18;
 
 // Export contracts.
 module.exports = function(deployer) {
   deployer.deploy(Auction);
-  // deployer.deploy(Auction1B1P, buyback, timestampEnd, wallet);
-  // deployer.deploy(Auction1B2P, buyback, timestampEnd, wallet);
-  // deployer.deploy(AuctionMM);
   deployer.deploy(SimpleStorage);
 };
--- test/microgrid.js
+++ test/microgrid.js
@@ -2,28 +2,23 @@
 // Description: Testing framework for microgrid using real data.
 
 // Contracts.
-const Auction1B2P = artifacts.require('Auction1B2P.sol');
-// Helper functions.
-const { expectThrow, increaseTime } = require('./helpers')
+var Auction = artifacts.require("./Auction.sol");
 
 // D3 JavaScript.
 const D3 = require("d3");
 
-////////////////////
-// Data analysis. //
-////////////////////
+///////////
+// Grid. //
+///////////
 
-// SunDance CSV format:
-// ["date", "use", "gen", "grid"].
+// Set up microgrid parameters.
 
-// Site object format:
-// {"name": file_name, "points": [points]}
+///////////
+// Data. //
+///////////
 
-// Point object format:
-// {"date": date, "use": use, "gen": gen, grid": grid}
-
-// SunDance.
-var sundance = [];
+// SunDance data.
+var sites = [];
 
 // Process SunDance data.
 for (var i = 1; i <= 100; i++) {
@@ -83,102 +78,51 @@ for (var i = 1; i <= 100; i++) {
       points: points
     }
 
-    // Add site to SunDance.
-    sundance.push(site);
+    // Add site to sites.
+    sites.push(site);
   })
 }
 
-// Log.
-// console.log(sundance);
+//////////
+// Bid. //
+//////////
 
-////////////////////////
-// Bidding Mechanism. //
-////////////////////////
+// Current date and time.
+var current = new Date();
+var currentYear = current.getFullYear();
+var currentMonth = current.getMonth() + 1;
+var currentDay = current.getDate();
+var currentHour = current.getHours();
+var currentMinute = current.getMinutes();
+var currentSecond = current.getSeconds();
 
-// Determine bids for given time interval.
+// Auction period: [year, month, day, hour].
+var nextHour = currentHour + 1;
+var nextAuction = [currentYear, currentMonth, currentDay, nextHour];
+var nextAuctionString =
+  currentMonth + "/" +
+  currentDay + "/" +
+  currentYear + " at " +
+  nextHour + ":00.";
 
-////////////////
-// Microgrid. //
-////////////////
+// Ascertain bids of each user.
 
-// Test microgrid setup using auction contract.
-contract('Auction1B2P', function (accounts) {
-  // Participants.
+///////////
+// Test. //
+///////////
 
-  // Seller.
-  let owner = accounts[0]
-  // Bidders.
-  let bidder1 = accounts[1]
-  let bidder2 = accounts[2]
-  let bidder3 = accounts[3]
-  let bidder4 = accounts[4]
-  let bidder5 = accounts[5]
-  let bidder6 = accounts[6]
-  let bidder7 = accounts[7]
-  let bidder8 = accounts[8]
-  let bidder9 = accounts[9]
-  let bidder10 = accounts[10]
-  // Winner.
-  let beneficiary = accounts[4]
+contract('Auction', function (accounts) {
 
-  // Time.
-  let day = 24 * 60 * 60;
-  let duration = 3 * day;
-  let auction;
-  let timestampEnd;
+  it("...should bid the value 5.", function() {
+    return Auction.deployed().then(function(instance) {
+      auctionInstance = instance;
 
-  // Tests.
-
-  beforeEach(async function() {
-    timestampEnd = web3.eth.getBlock('latest').timestamp  +  duration; // 1 hour from now
-    auction = await Auction1B2P.new(1e18, timestampEnd, beneficiary, {from: owner});
+      return auctionInstance.bid(5, {from: accounts[0]});
+    }).then(function() {
+      return auctionInstance.get.call();
+    }).then(function(value) {
+      assert.equal(value, 5, "The value 5 was not bid.");
+    });
   });
 
-  it('Should be able to set up the constructor auction', async function() {
-    assert.equal(await auction.owner(), owner, 'The owner is not set correctly')
-    //assert.equal(await auction.description(), "item", 'The description is not set correctly')
-    assert.equal(await auction.timestampEnd(), timestampEnd, 'The endtime is not set correctly')
-    assert.equal(await auction.beneficiary(), beneficiary, 'The beneficiary is not set correctly')
-  })
-
-  // where sdX refers to SunDance file X
-  it('Testing whole auction model', async function() {
-    for (i = 0; i < 10; i++) {
-
-      await auction.sendTransaction({ value: (sd1[i][1]*10^18), from: bidder1 });
-      await auction.sendTransaction({ value: (sd2[i][1]*10^18), from: bidder2 });
-      await auction.sendTransaction({ value: (sd3[i][1]*10^18), from: bidder3 });
-      await auction.sendTransaction({ value: (sd4[i][1]*10^18), from: bidder4 });
-      await auction.sendTransaction({ value: (sd5[i][1]*10^18), from: bidder5 });
-      await auction.sendTransaction({ value: (sd6[i][1]*10^18), from: bidder6 });
-      await auction.sendTransaction({ value: (sd7[i][1]*10^18), from: bidder7 });
-      await auction.sendTransaction({ value: (sd8[i][1]*10^18), from: bidder8 });
-      await auction.sendTransaction({ value: (sd9[i][1]*10^18), from: bidder9 });
-
-      // cannot withdraw before the end
-      await expectThrow(auction.finalize({ from: owner }));
-
-      increaseTime(duration + 1);
-
-      var balanceBefore = web3.eth.getBalance(beneficiary).toNumber()
-      var bidder1Before = web3.eth.getBalance(beneficiary).toNumber()
-
-      console.log(balanceBefore)
-
-      await auction.finalize({ from: owner });
-
-      var balanceAfter = web3.eth.getBalance(beneficiary).toNumber()
-      var winningPrice = balanceAfter-balanceBefore
-
-      assert.equal(balanceBefore + winningPrice, balanceAfter, "beneficiary didn't receive correct amount")
-
-      console.log(winningPrice)
-      console.log(await auction.winner())
-
-      //assert.equal(await auction.winner(), bidder9, "Winner not set up correctly");
-      //assert.equal(await auction.secondPlace(), bidder8, "Second Place not set up correctly");
-    }
-    // cannot withdraw more than once
-    await expectThrow(auction.finalize({ from: owner }));
-  });
 });
