89af2598c0e9f762aadfa1ca41d9dc73bc27fe2b
Parker Wild <parkerwild96@gmail.com>
2019-04-15 11:11:58 -0400

    MetaMask account change event.

--- src/layouts/auction/Auction.js
+++ src/layouts/auction/Auction.js
@@ -3,82 +3,6 @@ import React, { Component } from 'react'
 // UI Components
 import AuctionForm from './AuctionForm.js'
 
-////////////
-// Setup. //
-////////////
-
-// User object.
-function User(name, addr, data, use, gen, batt) {
-  this.name = name; // User name.
-  this.addr = addr; // Ethereum address.
-  this.data = data; // SunDance data file name.
-  this.use = use;   // Electricity usage for current time interval.
-  this.gen = gen;   // Electricity generation for current time
-  this.batt = batt; // Current battery level.
-}
-
-// Ethereum addresses hosted on Ganache.
-var addresses = [
-  "0x8E2077Ab0E6D14AF306106303d879d8b4F580e3f",
-  "0x301D2749B559BC9933b7bA85E5151f2b075DA5eB",
-  "0x4cE4f8F59B69474860d533191685edA6afA83B89",
-  "0x9956168BfD29AE6cadcDbe0A820b069f06Af5F7f",
-  "0xD73c86f2B6cbb14c01F4C634B461D20aa86BD7e1",
-  "0x3D43D1c22Ddd267AC0cE677AcA7c96f31b547234",
-  "0x42C8E0299Fd33b57Fa10628228bDfE1c6f013868",
-  "0x8328Bff14018a2Bd2498B5A04158ADb726185D60",
-  "0xfD2a5AFEE3A3c8C892Fed5A0CCfE705Fd06c6663",
-  "0x4917B088806dA204F406127520Cf06F8E82e17B9"
-];
-
-// Users.
-var users = [];
-for (var i = 0; i < 10; i++) {
-  // Create a new user.
-  users.push(
-    new User("user" + i, addresses[i], "SunDance_9" + i + ".csv", 0, 0, 0)
-  );
-}
-
-// Log.
-// console.log(users);
-
-// Battery capacities (Tesla Powerwall 2): kWh.
-const capacity = 13.5;
-
-// Microgrid statistics.
-var size = 10;
-
-// Electricity statistics.
-var grossUsage = 0; // kWh.
-var grossGeneration = 0; // kWh.
-var net = 0; // kWh.
-
-// Utility statistics: $/kWh.
-var utilityPrice = 10;
-var buyBackRate = 5;
-
-// Current date and time.
-var current = new Date();
-var currentYear = current.getFullYear();
-var currentMonth = current.getMonth() + 1;
-var currentDay = current.getDate();
-var currentHour = current.getHours();
-var currentMinute = current.getMinutes();
-var currentSecond = current.getSeconds();
-
-// Auction period: [year, month, day, hour].
-var nextHour = currentHour + 1;
-var nextAuction = [currentYear, currentMonth, currentDay, nextHour];
-var nextAuctionString =
-  currentMonth + "/" +
-  currentDay + "/" +
-  currentYear + " at " +
-  nextHour + ":00.";
-
-// Log.
-// console.log("Next auction: " + nextAuctionString);
-
 //////////////
 // Auction. //
 //////////////
@@ -100,7 +24,7 @@ class Auction extends Component {
           <h1>Auction</h1>
           <p><i>Specify your auction preferences here.</i></p>
 
-          <AuctionForm nextAuctionString={nextAuctionString}/>
+          <AuctionForm />
 
         </div>
       </div>
--- src/layouts/auction/AuctionForm.js
+++ src/layouts/auction/AuctionForm.js
@@ -6,9 +6,98 @@ import Web3 from 'web3'
 // Contracts.
 import SimpleStorageContract from '../../../build/contracts/SimpleStorage.json'
 
+////////////
+// Setup. //
+////////////
+
+// User object.
+function User(name, addr, data, use, gen, batt) {
+  this.name = name; // User name.
+  this.addr = addr; // Ethereum address.
+  this.data = data; // SunDance data file name.
+  this.use = use;   // Electricity usage for current time interval.
+  this.gen = gen;   // Electricity generation for current time
+  this.batt = batt; // Current battery level.
+}
+
+// Ethereum addresses hosted on Ganache.
+var addresses = [
+  "0x8E2077Ab0E6D14AF306106303d879d8b4F580e3f",
+  "0x301D2749B559BC9933b7bA85E5151f2b075DA5eB",
+  "0x4cE4f8F59B69474860d533191685edA6afA83B89",
+  "0x9956168BfD29AE6cadcDbe0A820b069f06Af5F7f",
+  "0xD73c86f2B6cbb14c01F4C634B461D20aa86BD7e1",
+  "0x3D43D1c22Ddd267AC0cE677AcA7c96f31b547234",
+  "0x42C8E0299Fd33b57Fa10628228bDfE1c6f013868",
+  "0x8328Bff14018a2Bd2498B5A04158ADb726185D60",
+  "0xfD2a5AFEE3A3c8C892Fed5A0CCfE705Fd06c6663",
+  "0x4917B088806dA204F406127520Cf06F8E82e17B9"
+];
+
+// Users.
+var users = [];
+for (var i = 0; i < 10; i++) {
+  // Create a new user.
+  users.push(
+    new User("user" + i, addresses[i], "SunDance_9" + i + ".csv", 0, 0, 0)
+  );
+}
+
+// Log.
+// console.log(users);
+
+// Battery capacities (Tesla Powerwall 2): kWh.
+const capacity = 13.5;
+
+// Microgrid statistics.
+var size = 10;
+
+// Electricity statistics.
+var grossUsage = 0; // kWh.
+var grossGeneration = 0; // kWh.
+var net = 0; // kWh.
+
+// Utility statistics: $/kWh.
+var utilityPrice = 10;
+var buyBackRate = 5;
+
+// Current date and time.
+var current = new Date();
+var currentYear = current.getFullYear();
+var currentMonth = current.getMonth() + 1;
+var currentDay = current.getDate();
+var currentHour = current.getHours();
+var currentMinute = current.getMinutes();
+var currentSecond = current.getSeconds();
+
+// Auction period: [year, month, day, hour].
+var nextHour = currentHour + 1;
+var nextAuction = [currentYear, currentMonth, currentDay, nextHour];
+var nextAuctionString =
+  currentMonth + "/" +
+  currentDay + "/" +
+  currentYear + " at " +
+  nextHour + ":00.";
+
+// Log.
+// console.log("Next auction: " + nextAuctionString);
+
+///////////////
+// MetaMask. //
+///////////////
+
+// Update MetaMask account on change.
+
+///////////////////
+// Auction form. //
+///////////////////
+
 class AuctionForm extends Component {
   constructor(props) {
     super(props);
+
+    // Set props.
+    // ...
 
     // Set state.
     this.state = {
@@ -16,6 +105,7 @@ class AuctionForm extends Component {
       bid: "[No bids have been submitted.]",
       bidder: "[No bidders have bidded.]",
       web3: null,
+      accounts: null,
       contract: null
     };
 
@@ -28,16 +118,35 @@ class AuctionForm extends Component {
   async componentDidMount() {
     try {
       // Web3.
-      var web3 = window.web3
+      var web3 = window.web3;
 
       // Use Mist/MetaMask's provider.
-      web3 = new Web3(web3.currentProvider)
+      web3 = new Web3(web3.currentProvider);
 
       // Enable MetaMask.
       await web3.currentProvider.enable();
 
       // NOTE: Acquire confirmation from the MetaMask account that this
       // application may view Ethereum addresses.
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Note: accounts is only populated by the default address selected in
+      // MetaMask. This can be accessed at "accounts[0]".
+
+      // MetaMask account change.
+      var selected = accounts[0];
+      var accountInterval = setInterval(async function() {
+        accounts = await web3.eth.getAccounts();
+        if (accounts[0] !== selected) {
+          selected = accounts[0];
+          this.setState(
+            { accounts },
+            this.updateInterface
+          );
+        }
+      }.bind(this), 100);
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
@@ -49,24 +158,14 @@ class AuctionForm extends Component {
 
       // Set web3, accounts, and contract to the state. Call runExample.
       this.setState(
-        {
-          web3,
-          contract: instance
-        },
-        this.runExample
+        { web3, accounts, contract: instance },
+        this.initialize
       );
     } catch (error) {
       // Throw error.
       alert(`Failed to load web3, accounts, or contract.`);
       console.error(error);
     }
-  }
-
-  handleChange(event) {
-    // Set state.
-    this.setState({
-      value: event.target.value
-    });
   }
 
   // Submit bid.
@@ -85,9 +184,6 @@ class AuctionForm extends Component {
     // Use web3 to get the user's accounts.
     const accounts = await this.state.web3.eth.getAccounts();
 
-    // Note: accounts is only populated by the default address selected in
-    // MetaMask. This can be accessed at "accounts[0]".
-
     // Stores a given value.
     await contract.methods.set(this.state.value).send({
       from: accounts[0]
@@ -98,26 +194,47 @@ class AuctionForm extends Component {
 
     // Set state.
     this.setState({
+      accounts: accounts,
       bid: response,
       bidder: accounts[0]
     });
   }
 
-  // End auction.
-  results() {
-    // Log.
-    console.log("Auction results requested.");
+  handleChange(event) {
+    // Set state.
+    this.setState({
+      value: event.target.value
+    });
   }
 
-  // Called after component mounts.
-  async runExample() {
+  // MetaMask account changed.
+  updateInterface() {
+    // Log.
+    console.log("MetaMask account changed.");
+  }
+
+  // Initialize auction.
+  async initialize() {
     try  {
       // Log.
       console.log("Auction initialized.")
 
     } catch (error) {
       // Throw error.
-      alert(`Failed to run example function.`);
+      alert(`Failed to initalize auction.`);
+      console.error(error);
+    }
+  }
+
+  // Finalize auction.
+  async finalize() {
+    try  {
+      // Log.
+      console.log("Auction finalized.");
+
+    } catch (error) {
+      // Throw error.
+      alert(`Failed to finalize auction.`);
       console.error(error);
     }
   }
@@ -133,7 +250,7 @@ class AuctionForm extends Component {
       <div>
 
         <h2>Start an Auction</h2>
-        <p>The next auction will run on {this.props.nextAuctionString}</p>
+        <p>The next auction will run on {nextAuctionString}</p>
 
         <h2>Make a Bid</h2>
         <p>Input bid ($) here.</p>
@@ -152,7 +269,7 @@ class AuctionForm extends Component {
         <h2>End an Auction</h2>
         <p>See the auction results here.</p>
 
-        <button onClick={() => {this.results()}}>End Auction</button>
+        <button onClick={() => {this.finalize()}}>End Auction</button>
 
       </div>
 
